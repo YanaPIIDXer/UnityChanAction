75f8cc2fc8b34009e33291299ec4f67b9522d431
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-13 22:49:25 +0900

    TPSカメラ

---
+++ Assets/Scripts/CameraControl/PlayerCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraControl
+{
+    /// <summary>
+    /// プレイヤーカメラクラス
+    /// </summary>
+    public class PlayerCamera : MonoBehaviour
+    {
+        /// <summary>
+        /// プレイヤーTransform
+        /// TODO:PlayerをPrefabからInstantiateするような作りになる場合はまた別途考える
+        /// </summary>
+        [SerializeField]
+        private Transform playerTransform = null;
+
+        /// <summary>
+        /// 視点の高さオフセット
+        /// </summary>
+        private static readonly float HeightOffset = 1.2f;
+
+        /// <summary>
+        /// プレイヤーからの距離
+        /// </summary>
+        private static readonly float distance = 0.7f;
+
+        void Update()
+        {
+            var lookAt = playerTransform.position + Vector3.up * HeightOffset;
+            transform.position = lookAt - Vector3.forward * distance;
+            transform.LookAt(lookAt);
+        }
+    }
+}
