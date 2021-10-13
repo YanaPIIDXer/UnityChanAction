7d40bcde0d7d49aab61af0d258e716d24837d1e6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-13 22:43:02 +0900

    プレイヤー移動クラス

---
+++ Assets/Scripts/Character/Player/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Zenject;
+
+namespace Character.Player
+{
+    /// <summary>
+    /// プレイヤー移動Component
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMove : MonoBehaviour
+    {
+        /// <summary>
+        /// Rigidbody
+        /// </summary>
+        private Rigidbody rigidBody = null;
+
+        /// <summary>
+        /// 移動ベクトル
+        /// </summary>
+        private Vector2 moveVector = Vector2.zero;
+
+        /// <summary>
+        /// IPlayerControlインタフェースの注入
+        /// </summary>
+        /// <param name="playerControl">IPlayerControlインタフェース</param>
+        [Inject]
+        public void InjectPlayerControl(IPlayerControl playerControl)
+        {
+            playerControl.Move.Subscribe(v => moveVector = v).AddTo(gameObject);
+        }
+
+        void Awake()
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            rigidBody.velocity = new Vector3(moveVector.x, rigidBody.velocity.y, moveVector.y);
+            transform.LookAt(transform.position + new Vector3(moveVector.x, 0.0f, moveVector.y));
+        }
+    }
+}
