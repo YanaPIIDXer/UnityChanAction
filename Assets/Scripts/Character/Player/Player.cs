93721387f5aa8808e03deb385a6623ef3e481bf9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-13 22:53:18 +0900

    プレイヤーアニメーションクラス

--- Assets/Scripts/Character/Player/Player.cs
+++ Assets/Scripts/Character/Player/Player.cs
@@ -13,12 +13,18 @@ namespace Character.Player
         /// 移動Component
         /// </summary>
         PlayerMove Move { get; }
+
+        /// <summary>
+        /// アニメーションComponent
+        /// </summary>
+        PlayerAnimation Animation { get; }
     }
 
     /// <summary>
     /// プレイヤークラス
     /// </summary>
     [RequireComponent(typeof(PlayerMove))]
+    [RequireComponent(typeof(PlayerAnimation))]
     public class Player : MonoBehaviour, ICharacter, IPlayerFacade
     {
         /// <summary>
@@ -26,9 +32,16 @@ namespace Character.Player
         /// </summary>
         public PlayerMove Move { get; private set; }
 
+        /// <summary>
+        /// アニメーションComponent
+        /// </summary>
+        /// <returns></returns>
+        public PlayerAnimation Animation { get; private set; }
+
         void Awake()
         {
             Move = GetComponent<PlayerMove>();
+            Animation = GetComponent<PlayerAnimation>();
         }
     }
 }
---
+++ Assets/Scripts/Character/Player/PlayerAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Zenject;
+
+namespace Character.Player
+{
+    /// <summary>
+    /// プレイヤーのアニメーションクラス
+    /// </summary>
+    [RequireComponent(typeof(Animator))]
+    public class PlayerAnimation : MonoBehaviour
+    {
+        /// <summary>
+        /// Animator
+        /// </summary>
+        private Animator animator = null;
+
+        /// <summary>
+        /// IPlayerControlインタフェースの注入
+        /// </summary>
+        /// <param name="playerControl">IPlayerControlインタフェース</param>
+        [Inject]
+        public void InjectPlayerControl(IPlayerControl playerControl)
+        {
+            playerControl.Move.Select(v => v.sqrMagnitude > 0.0f).Subscribe(bIsMove => animator.SetBool("IsMoving", bIsMove)).AddTo(gameObject);
+        }
+
+        void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+}
