a97d40970f5bb754b69994ddf2c99d6f60ecc85e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-13 22:28:17 +0900

    プレイヤー制御インタフェース定義

---
+++ Assets/Scripts/Character/Player/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+namespace Character.Player
+{
+    /// <summary>
+    /// プレイヤー制御インタフェース
+    /// </summary>
+    public interface IPlayerControl
+    {
+        /// <summary>
+        /// 移動
+        /// </summary>
+        IObservable<Unit> Move { get; }
+
+        /// <summary>
+        /// スキル
+        /// </summary>
+        IObservable<int> Skill { get; }
+    }
+}
