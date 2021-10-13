0fa3373e28d1262051c4379736575de3f0c485ff
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-14 03:24:30 +0900

    移動Component

--- Assets/Scripts/Character/Enemy/Enemy.cs
+++ Assets/Scripts/Character/Enemy/Enemy.cs
@@ -9,12 +9,26 @@ namespace Character.Enemy
     /// </summary>
     public interface IEnemy
     {
+        /// <summary>
+        /// 移動Component
+        /// </summary>
+        EnemyMove Move { get; }
     }
 
     /// <summary>
     /// エネミークラス
     /// </summary>
+    [RequireComponent(typeof(EnemyMove))]
     public class Enemy : MonoBehaviour, ICharacter, IEnemy
     {
+        /// <summary>
+        /// 移動Component
+        /// </summary>
+        public EnemyMove Move { get; private set; }
+
+        void Awake()
+        {
+            Move = GetComponent<EnemyMove>();
+        }
     }
 }
---
+++ Assets/Scripts/Character/Enemy/EnemyMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character.Enemy
+{
+    /// <summary>
+    /// 敵の移動
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody))]
+    public class EnemyMove : MonoBehaviour
+    {
+        /// <summary>
+        /// Rigidbody
+        /// </summary>
+        private Rigidbody rigidBody = null;
+
+        /// <summary>
+        /// 移動ベクトル
+        /// </summary>
+        public Vector2 MoveVector { set; private get; }
+
+        /// <summary>
+        /// 力
+        /// </summary>
+        private Vector3? force = null;
+
+        /// <summary>
+        /// 力を加える
+        /// </summary>
+        /// <param name="force">加える力</param>
+        public void AddForce(Vector3 force)
+        {
+            this.force = force;
+        }
+
+        void Awake()
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            if (force != null)
+            {
+                rigidBody.AddForce(force.Value, ForceMode.Force);
+                force = null;
+            }
+
+            rigidBody.velocity = new Vector3(MoveVector.x, rigidBody.velocity.y, MoveVector.y);
+            transform.LookAt(transform.position + new Vector3(MoveVector.x, 0.0f, MoveVector.y));
+        }
+    }
+}
