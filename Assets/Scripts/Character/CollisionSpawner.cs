5d594d3d6c11c40df3b095aeb3f680ce7cf4fd3f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-14 04:39:48 +0900

    コリジョン生成Component

---
+++ Assets/Scripts/Character/CollisionSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Master;
+
+namespace Character
+{
+    /// <summary>
+    /// コリジョン生成
+    /// </summary>
+    public class CollisionSpawner : MonoBehaviour
+    {
+        /// <summary>
+        /// データリスト
+        /// </summary>
+        private List<CollisionData> dataList = null;
+
+        /// <summary>
+        /// 経過時間
+        /// </summary>
+        private float elapsedTime = 0.0f;
+
+        /// <summary>
+        /// スキル開始時に呼び出す
+        /// </summary>
+        /// <param name="skillId">スキルＩＤ</param>
+        public void OnStartSkill(int skillId)
+        {
+            dataList = CollisionMaster.Collect(skillId);
+            elapsedTime = 0.0f;
+        }
+
+        void Update()
+        {
+            elapsedTime += Time.deltaTime;
+
+            for (int i = dataList.Count - 1; i >= 0; i--)
+            {
+                if (elapsedTime >= dataList[i].SpawnTime)
+                {
+                    Debug.Log("TODO:コリジョン生成処理");
+                    dataList.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
