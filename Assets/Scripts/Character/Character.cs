aa1ec24aa2979692a4046bbbad94467f448338a9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-13 22:24:50 +0900

    キャラクタインタフェース定義

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character
+{
+    /// <summary>
+    /// キャラクタインタフェース
+    /// </summary>
+    public interface ICharacter
+    {
+    }
+}
