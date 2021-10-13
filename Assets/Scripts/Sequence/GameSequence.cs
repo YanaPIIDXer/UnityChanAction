b4decae87f729b20561745baa8ba81e8dec0ee62
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-13 23:47:05 +0900

    ゲームシーケンスクラス

---
+++ Assets/Scripts/Sequence/GameSequence.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Master;
+
+namespace Sequence
+{
+    /// <summary>
+    /// ゲームシーケンス
+    /// </summary>
+    public class GameSequence : MonoBehaviour
+    {
+        void Awake()
+        {
+            LoadMasterData();
+        }
+
+        /// <summary>
+        /// マスタデータの読み込み
+        /// </summary>
+        private void LoadMasterData()
+        {
+            SkillMaster.Load();
+        }
+    }
+}
