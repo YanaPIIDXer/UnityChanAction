6d7c61d1626609ddca4ee1b7047d2edbef8be165
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-14 06:32:50 +0900

    コリジョンヒット時の反応の種類定数定義

---
+++ Assets/Scripts/Collision/ReactionType.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Collision
+{
+    /// <summary>
+    /// コリジョンヒット時の反応の種類
+    /// </summary>
+    public static class ReactionType
+    {
+        /// <summary>
+        /// 何も無し
+        /// </summary>
+        public static readonly byte None = 0;
+
+        /// <summary>
+        /// 吹っ飛び
+        /// </summary>
+        public static readonly byte Blow = 1;
+
+        /// <summary>
+        /// 打ち上げ
+        /// </summary>
+        public static readonly byte Lift = 2;
+    }
+}
