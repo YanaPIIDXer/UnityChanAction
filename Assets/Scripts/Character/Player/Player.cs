a66fd0d33a193d094d42f2d865c0edda9e96dd9f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-13 22:44:41 +0900

    PlayerFacadeインタフェース経由で移動Componentを取れるようにした

--- Assets/Scripts/Character/Player/Player.cs
+++ Assets/Scripts/Character/Player/Player.cs
@@ -9,12 +9,26 @@ namespace Character.Player
     /// </summary>
     public interface IPlayerFacade
     {
+        /// <summary>
+        /// 移動Component
+        /// </summary>
+        PlayerMove Move { get; }
     }
 
     /// <summary>
     /// プレイヤークラス
     /// </summary>
+    [RequireComponent(typeof(PlayerMove))]
     public class Player : MonoBehaviour, ICharacter, IPlayerFacade
     {
+        /// <summary>
+        /// 移動Component
+        /// </summary>
+        public PlayerMove Move { get; private set; }
+
+        void Awake()
+        {
+            Move = GetComponent<PlayerMove>();
+        }
     }
 }
