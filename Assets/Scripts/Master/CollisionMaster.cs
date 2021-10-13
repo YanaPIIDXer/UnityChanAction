015c4d30d62707a8629d81298231bc1a2521890d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-14 04:33:28 +0900

    コリジョンマスタ

---
+++ Assets/Scripts/Master/CollisionMaster.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Master
+{
+    public class CollisionMaster
+    {
+        /// <summary>
+        /// バイナリのパス
+        /// </summary>
+        private static readonly string BinaryPath = "Master/CollisionMaster";
+
+        /// <summary>
+        /// データリスト
+        /// </summary>
+        private CollisionData[] datas = null;
+
+        /// <summary>
+        /// 指定したスキルＩＤのものを列挙
+        /// </summary>
+        /// <param name="skillId">スキルＩＤ</param>
+        /// <returns>データリスト</returns>
+        public static List<CollisionData> Collect(int skillId)
+        {
+            List<CollisionData> list = new List<CollisionData>();
+            foreach (var data in instance.datas)
+            {
+                if (data.SkillId == skillId)
+                {
+                    list.Add(data);
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 読み込み
+        /// </summary>
+        public static void Load()
+        {
+            var textAsset = Resources.Load<TextAsset>(BinaryPath);
+            byte[] binary = textAsset.bytes;
+            instance.datas = CollisionData.SerializeAll(binary);
+        }
+
+        #region Singleton
+        private CollisionMaster() { }
+        private static CollisionMaster instance = new CollisionMaster();
+        #endregion
+
+    }
+}
---
+++ Assets/Scripts/Master/Data/CollisionData.cs
@@ -0,0 +1,55 @@
+using Stream;
+
+namespace Master
+{
+	public class CollisionData
+	{
+		public int SkillId { get; private set; }
+		public float SpawnTime { get; private set; }
+		public float LifeTime { get; private set; }
+		public float Radius { get; private set; }
+		public int Power { get; private set; }
+		public int ReactionType { get; private set; }
+		public float ReactionPower { get; private set; }
+
+		public void Serialize(IMemoryStream stream)
+		{
+			int SkillId = new int();
+			stream.Serialize(ref SkillId);
+			this.SkillId = SkillId;
+			float SpawnTime = new float();
+			stream.Serialize(ref SpawnTime);
+			this.SpawnTime = SpawnTime;
+			float LifeTime = new float();
+			stream.Serialize(ref LifeTime);
+			this.LifeTime = LifeTime;
+			float Radius = new float();
+			stream.Serialize(ref Radius);
+			this.Radius = Radius;
+			int Power = new int();
+			stream.Serialize(ref Power);
+			this.Power = Power;
+			int ReactionType = new int();
+			stream.Serialize(ref ReactionType);
+			this.ReactionType = ReactionType;
+			float ReactionPower = new float();
+			stream.Serialize(ref ReactionPower);
+			this.ReactionPower = ReactionPower;
+		}
+
+		public static CollisionData[] SerializeAll(byte[] buffer)
+		{
+			MemoryStreamReader reader = new MemoryStreamReader(buffer);
+			int size = 0;
+			reader.Serialize(ref size);
+			CollisionData[] datas = new CollisionData[size];
+			for (int i = 0; i < size; i++)
+			{
+				CollisionData data = new CollisionData();
+				data.Serialize(reader);
+				datas[i] = data;
+			}
+			return datas;
+		}
+	}
+}
