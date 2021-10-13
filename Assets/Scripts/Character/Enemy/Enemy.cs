03e4f62b7563720981e7af79af406914aa74ba52
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-14 03:18:55 +0900

    エネミークラス定義

---
+++ Assets/Scripts/Character/Enemy/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character.Enemy
+{
+    /// <summary>
+    /// 各Componentにアクセスするためのインタフェース
+    /// </summary>
+    public interface IEnemy
+    {
+    }
+
+    /// <summary>
+    /// エネミークラス
+    /// </summary>
+    public class Enemy : MonoBehaviour, ICharacter, IEnemy
+    {
+    }
+}
