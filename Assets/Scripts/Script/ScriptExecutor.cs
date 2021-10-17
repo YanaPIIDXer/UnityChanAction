f8ce2af16f6336d0f02b4eaee01da172836202ad
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-17 22:22:15 +0900

    MoonSharp導入

---
+++ Assets/Scripts/Script/ScriptExecutor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script
+{
+    /// <summary>
+    /// スクリプト実行クラス
+    /// </summary>
+    public class ScriptExecutor
+    {
+    }
+}
